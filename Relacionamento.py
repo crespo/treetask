86e9abc1c7b9c69b365473be07e5feb32a085157
raul crespo <raulxcrespo@gmail.com>
2024-05-30 16:44:08 -0300

    fix: flags nopep8 para evitar quebra de linha

--- Relacionamento.py
+++ Relacionamento.py
@@ -8,31 +8,30 @@ class Relacionamento:
     def adicionar_usuario(self, usuario_id, nome):
         if usuario_id not in self.usuarios:
             self.usuarios[usuario_id] = Usuario(usuario_id, nome)
+            print(f"Usuário {usuario_id} adicionado com Sucesso!")
         else:
             print(f"Usuario {usuario_id} já existe.")
 
     def adicionar_relacionamento(self, usuario_id1, usuario_id2):
         if usuario_id1 in self.usuarios and usuario_id2 in self.usuarios:
-            self.usuarios[usuario_id1].relacionamentos.append(
-                self.usuarios[usuario_id2])
-            self.usuarios[usuario_id2].relacionamentos.append(
-                self.usuarios[usuario_id1])
+            self.usuarios[usuario_id1].relacionamentos.append(self.usuarios[usuario_id2])  # nopep8
+            self.usuarios[usuario_id2].relacionamentos.append(self.usuarios[usuario_id1])  # nopep8
+            print(f"Relacionamento entre {usuario_id1} e {usuario_id2} adicionado com Sucesso!")  # nopep8
         else:
-            print(f"Um ou ambos usuários não achados: {
-                  usuario_id1}, {usuario_id2}")
+            print(f"Um ou ambos usuários não achados: {usuario_id1}, {usuario_id2}")  # nopep8
 
     def remover_usuario(self, usuario_id):
         if usuario_id in self.usuarios:
             for amigo in self.usuarios[usuario_id].relacionamentos:
                 amigo.relacionamentos.remove(self.usuarios[usuario_id])
             del self.usuarios[usuario_id]
+            print(f"Usuário {usuario_id} removido com sucesso!")
         else:
             print(f"Usuario {usuario_id} não encontrado.")
 
     def mostrar_arvore(self):
         for usuario in self.usuarios.values():
-            print(f"{usuario.nome} ({
-                usuario.usuario_id}) -> {[amigo.nome for amigo in usuario.relacionamentos]}")
+            print(f"{usuario.nome} ({usuario.usuario_id}) -> {[amigo.nome for amigo in usuario.relacionamentos]}")  # nopep8
 
     def encontrar_usuario(self, usuario_id):
         if usuario_id in self.usuarios:
